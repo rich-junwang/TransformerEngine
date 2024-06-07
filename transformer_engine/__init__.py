a058329a217b876385288e88b9aaa22a844c2b19
Jun Wang <juwanga@amazon.com>
2024-06-07 10:43:15 -0700

    fix for pytorch import error

--- transformer_engine/__init__.py
+++ transformer_engine/__init__.py
@@ -9,29 +9,6 @@
 from importlib import metadata
 import transformer_engine.common
 
-try:
-    from . import pytorch
-except (ImportError, StopIteration) as e:
-    pass
 
-try:
-    from . import jax
-except (ImportError, StopIteration) as e:
-    pass
-
-try:
-    from . import paddle
-except (ImportError, StopIteration) as e:
-    pass
-
-try:
-    import transformer_engine_jax
-except ImportError:
-    pass
-
-try:
-    import transformer_engine_paddle
-except ImportError:
-    pass
 
 __version__ = str(metadata.version("transformer_engine"))
